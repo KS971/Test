2bfc326cf9923ee4ebab7e42d2f9ab2b756c24d8
Kang <sanddara@naver.com>
2020-05-26 21:11:20 +0900

    TEST test1 test2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> list = null;
+        
+        list.add("a");
+
+        for(String s : list) {
+            System.out.println(s);
+        }
+        
+        
     }
 }
